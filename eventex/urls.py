a5afde4a0021a0f17ece18c46f093bef36deec5c
Raffael Tancman <rtancman@gmail.com>
2016-01-07 18:14:27 -0200

    fix app core, incluindo index.html

--- eventex/urls.py
+++ eventex/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'eventex.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'eventex.core.views.home'),
     url(r'^admin/', include(admin.site.urls)),
 ]
