b254f00a3c08e59cb91b550de7b65b4583acdad9
Raffael Tancman <rtancman@gmail.com>
2016-01-20 20:52:38 -0200

    test para app subscriptions, alterando html do form

--- eventex/subscriptions/test.py
+++ eventex/subscriptions/test.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from eventex.subscriptions.forms import SubscriptionForm
+
 
 class SubscriptionsTest(TestCase):
     def setUp(self):
@@ -21,7 +23,32 @@ class SubscriptionsTest(TestCase):
         Html must contain input tags
         """
         self.assertContains(self.resp, '<form')
-        self.assertContains(self.resp, '<input', 5)
+        self.assertContains(self.resp, '<input', 6)
         self.assertContains(self.resp, 'type="text"', 3)
         self.assertContains(self.resp, 'type="email"')
         self.assertContains(self.resp, 'type="submit"')
+
+    def test_csrf(self):
+        """
+        Html must contain csfr
+        """
+        self.assertContains(self.resp, 'csrfmiddlewaretoken')
+        """
+        Context must have subscription form
+        """
+        form = self.resp.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
+
+    def test_has_form(self):
+        """
+        Context must have subscription form
+        """
+        form = self.resp.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
+
+    def test_form_has_fields(self):
+        """
+        Form must have 4 fields
+        """
+        form = self.resp.context['form']
+        self.assertSequenceEqual(['name', 'cpf', 'email', 'phone'], list(form.fields))
