6d0fc323b25e9d8825663d3294709d37ccbef01b
Raffael Tancman <rtancman@gmail.com>
2016-01-21 14:13:30 -0200

    adiciona link para inscricao

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -2,16 +2,19 @@ from django.test import TestCase
 
 class HomeTest(TestCase):
     def setUp(self):
-        self.response = self.client.get('/')
+        self.resp = self.client.get('/')
 
     def test_get(self):
         """
         GET / must return status code 200
         """
-        self.assertEquals(200,self.response.status_code)
+        self.assertEquals(200,self.resp.status_code)
 
     def test_template(self):
         """
         must use index.html
         """
-        self.assertTemplateUsed(self.response, "index.html")
+        self.assertTemplateUsed(self.resp, "index.html")
+
+    def test_subscription_link(self):
+        self.assertContains(self.resp, 'href="/inscricao/"')
